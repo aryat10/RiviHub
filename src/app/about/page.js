756067adeb57881a8e74abfc535cd9b6a0a8067b
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-04-20 01:09:03 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,12 @@
 import React from "react";
-
-const Page = () =>{
+import Herosection from "../Component/Herosection";
+const About = () =>{
     return(
     <>
-      <h1> About page</h1>
+      {/* <h1> About page</h1>   */}
+      <Herosection title = {'Our Story'} imageUrl = {'/about1.svg'} />
     </>
     )
 }
 
-export default Page;
+export default About;
